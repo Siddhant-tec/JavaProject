a53e39a150d93df97e9ea4db42a3a053bc7f61ce
Siddhant-tec <siddhanttiwari200@gmail.com>
2022-05-01 11:25:29 +0530

    added programs from certification course

---
+++ src/StringMethods2.java
@@ -0,0 +1,12 @@
+public class StringMethods2 {
+    public static void main(String[] args){
+        String myName = "Siddhant Tiwari";
+        String upperName = myName.toUpperCase();
+        String lowerName = myName.toLowerCase();
+
+        String lastname = myName.substring(9);
+        int whereisT = myName.indexOf("t");
+
+        System.out.println(upperName + " " + lowerName + " " + lastname + " " + whereisT);
+    }
+}
