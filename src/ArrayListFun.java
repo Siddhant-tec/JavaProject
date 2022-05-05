da9703b22a774a455ecb366444406c7f40ab92fa
Siddhant-tec <siddhanttiwari200@gmail.com>
2022-05-05 14:56:35 +0530

    added programs part of LTI training module

--- src/ArrayListFun.java
+++ src/ArrayListFun.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ArrayListFun {
     public static void main(String[] args){
         ArrayList<String> nameList = new ArrayList<>();
@@ -9,7 +13,15 @@ public class ArrayListFun {
         nameList.add("Pam");
         nameList.add("Angela");
         nameList.forEach((n) -> System.out.println(n));
+        System.out.println();
 
+        nameList.sort(String::compareToIgnoreCase);
+
+        for(String name: nameList){
+            System.out.println(name);
+        }
+        System.out.println();
+        Collections.reverse(nameList);
         for(String name: nameList){
             System.out.println(name);
         }
---
+++ src/ItemType.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class ItemType {
+    private String name;
+    private double  deposit;
+    private double costPerDay;
+
+    public ItemType(){
+
+    }
+
+    public ItemType(String name, double deposit, double costPerDay){
+        this.name = name;
+        this.deposit = deposit;
+        this.costPerDay = costPerDay;
+
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getDeposit(){
+        return deposit;
+    }
+
+    public double getCostPerDay(){
+        return costPerDay;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public void setCostPerDay(double costPerDay){
+        this.costPerDay = costPerDay;
+    }
+
+}
+
+class ItemTypeDemo{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<ItemType> itemList = new ArrayList<>();
+        int input = s.nextInt();
+        s.nextLine();
+        ItemType temp;
+        while(input --> 0){
+            temp = new ItemType(s.nextLine(), Double.parseDouble(s.nextLine()), Double.parseDouble(s.nextLine()));
+            //itemList.add(new ItemType(s.nextLine(), Double.parseDouble(s.nextLine()), Double.parseDouble(s.nextLine())));
+            itemList.add(temp);
+        }
+        Collections.sort(itemList, Comparator.comparing(ItemType::getCostPerDay));
+
+        for(ItemType a : itemList){
+            System.out.println(a.getName() + " " +  a.getCostPerDay() + " " + a.getDeposit());
+        }
+    }
+
+}
+
